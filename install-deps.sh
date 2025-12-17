1071d6a39243d2d32f8715adf8f6ceeedcf135ac
Maxim Krait <100289164+MaximKrait@users.noreply.github.com>
2025-12-18 01:19:12 +0400

    Add SDL2_image and SDL2_mixer dependencies

--- install-deps.sh
+++ install-deps.sh
@@ -28,18 +28,17 @@ install_pkg() {
     case "$PM" in
         apt)
             sudo apt update
-            sudo apt install -y libsdl2-dev libsdl2-ttf-dev libsdl2-gfx-dev
+            sudo apt install -y libsdl2-dev libsdl2-ttf-dev libsdl2-gfx-dev libsdl2-image-dev libsdl2-mixer-dev
             ;;
         dnf)
             sudo dnf update
-            sudo dnf install -y SDL2-devel SDL2_ttf-devel SDL2_gfx-devel
+            sudo dnf install -y SDL2-devel SDL2_ttf-devel SDL2_gfx-devel SDL2_image-devel SDL2_mixer-devel
             ;;
         pacman)
-            sudo pacman update
-            sudo pacman -Sy --noconfirm sdl2 sdl2_ttf sdl2_gfx
+            sudo pacman -Sy --noconfirm sdl2 sdl2_ttf sdl2_gfx sdl2_image sdl2_mixer
             ;;
         zypper)
-            sudo zypper install -y libSDL2-devel libSDL2_ttf-devel SDL2_gfx-devel
+            sudo zypper install -y libSDL2-devel libSDL2_ttf-devel SDL2_gfx-devel SDL2_image-devel SDL2_mixer-devel
             ;;
         *)
             return 1
